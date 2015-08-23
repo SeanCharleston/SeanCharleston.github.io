0a1de93f31b8f52611b5c8c1133e9bd31b00e138
Sean Charleston <sean.andrew.charleston@gmail.com>
2015-08-23 18:01:48 +1200

    Updated the skills dropdown using jquery (animated)

--- cv/cv.js
+++ cv/cv.js
@@ -1,14 +1,5 @@
-
-if (document.getElementById){
-	document.writeln('<style type="text/css"><!--')
-	document.writeln('.texter {display:none} @media print {.texter {display:block;}}')
-	document.writeln('//--></style>')
-}
-
-function openClose(theID){
-	if (document.getElementById(theID).style.display == "block"){
-		document.getElementById(theID).style.display = "none"
-	}else{
-		document.getElementById(theID).style.display = "block" 
-	}
-}
+$(document).ready(function(){
+    $(".parentText").click(function(){
+        $(this).parent().find(".childText").slideToggle("slow");
+    });
+});
